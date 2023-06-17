b4d4ed5c7907d0ebcbed5a7d6c40528cd926cbb0
[junhao yong] <[junhao.yong@aakel.co.jp]>
2023-06-17 15:11:18 +0900

    add idea

--- mosa_qusishack/API/openapi.py
+++ mosa_qusishack/API/openapi.py
@@ -8,48 +8,67 @@ import re
 
 def main():
 
+    #get input from user
     parser = argparse.ArgumentParser()
     parser.add_argument("--input", "-i", type=str, required=True)
     args = parser.parse_args()
     user_input = args.input
+    user_input_list = user_input.split()
+    level_input = user_input_list[0]
+    role_input = user_input_list[1]
+    language_input = user_input_list[2]
+    
+    # programming_language = ["Python","PHP","JavaScript",""]
+    # level = ["Easy","Intermediate","Difficult"]
+    # roles = ["Backend","Frontend","Fullstack","ML Engineer","Data Engineer","Security Engineer","DevOps Engineer"]
     
     print(f'User input: {user_input}')
-    branding_snippet = generate_branding_snippet(user_input)
-    branding_keyword = generate_keyword(user_input)
-    print(branding_snippet)
-    print(branding_keyword)
+    project_ideas = [generate_project_ideas(level_input, role_input) for _ in range(0,3)]
+    tutorial_links = [generate_tutorial_link(idea, language_input) for idea in project_ideas]
+    print(f"Ideas:{project_ideas}")
+    print(f"Useful Links:{tutorial_links}")
 
-def generate_branding_snippet(user_input: str):
+def generate_project_ideas(level_input: str, role_input: str) -> str :
     
     openai.api_key = os.environ.get("API_Key")
  
-    prompt = f"Generate upbeat branding snippet for {user_input}"
-
-    response = openai.Completion.create(model="text-davinci-003", prompt=prompt, temperature=1.5, max_tokens=32)
-
-    branding_text: str = response["choices"][0]["text"]
+    prompt = f"Suggest one {level_input} portfolio project to be a {role_input} engineer(only project name)"
+    #Generate response
+    response = openai.Completion.create(model="text-davinci-003", 
+                                        prompt=prompt, 
+                                        temperature=1,
+                                        max_tokens=256,
+                                        top_p=1,
+                                        best_of=3,
+                                        frequency_penalty=0,
+                                        presence_penalty=0)
     
-    branding_text = branding_text.strip()
+    portfolio_idea: str = response["choices"][0]["text"]
+    portfolio_idea = portfolio_idea.strip()
+    portfolio_idea = portfolio_idea.replace("1.","")
+    portfolio_idea = portfolio_idea.lower()
     
-    last_char =  branding_text[-1]
-    if last_char not in {"!",".","?"}:
-        branding_text += "..."
+    return portfolio_idea
 
-    return branding_text
-
-def generate_keyword(user_input: str):
+def generate_tutorial_link(project_example: str, language_input: str) -> str :
     
     openai.api_key = os.environ.get("API_Key")
- 
-    enriched_prompt = f"Generate related branding keywords for {user_input}:"
-
-    response = openai.Completion.create(model="text-davinci-003", prompt=enriched_prompt, temperature=1.5, max_tokens=32)
-
-    keywords_text: str = response["choices"][0]["text"]
     
-    keywords_text = keywords_text.strip()
+    #Generate response
+    enriched_prompt = f"Suggest one best resource to learn how to build a {project_example} using {language_input}(link only)"
+    print(enriched_prompt)
+    response = openai.Completion.create(model="text-davinci-003",
+                                        prompt=enriched_prompt,
+                                        temperature=1,
+                                        max_tokens=256,
+                                        top_p=1,
+                                        frequency_penalty=0,
+                                        presence_penalty=0)
 
-    return keywords_text
+    tutorial_link: str = response["choices"][0]["text"]
+    tutorial_link = tutorial_link.strip()
+
+    return tutorial_link
 
 if __name__ == "__main__" :
     main()
