ae3c4444cbc9771cb8e502726ecbf7dbc524e5bd
[junhao yong] <[junhao.yong@aakel.co.jp]>
2023-06-17 11:40:41 +0900

    add infra

--- mosa_qusishack/API/openapi.py
+++ mosa_qusishack/API/openapi.py
@@ -1,6 +1,10 @@
 import os
+from dotenv import load_dotenv
+load_dotenv()
+
 import openai
 import argparse
+import re
 
 def main():
 
@@ -10,18 +14,42 @@ def main():
     user_input = args.input
     
     print(f'User input: {user_input}')
-    generate_answer(user_input)
+    branding_snippet = generate_branding_snippet(user_input)
+    branding_keyword = generate_keyword(user_input)
+    print(branding_snippet)
+    print(branding_keyword)
 
-def generate_answer(user_input: str):
+def generate_branding_snippet(user_input: str):
     
-    openai.api_key = os.getenv("OPENAI_API_KEY")
+    openai.api_key = os.environ.get("API_Key")
  
     prompt = f"Generate upbeat branding snippet for {user_input}"
 
-    response = openai.Completion.create(model="text-davinci-003", prompt=prompt, temperature=0, max_tokens=32)
+    response = openai.Completion.create(model="text-davinci-003", prompt=prompt, temperature=1.5, max_tokens=32)
 
-    print(response)
-    answer = response["choices"][0]["text"]
+    branding_text: str = response["choices"][0]["text"]
+    
+    branding_text = branding_text.strip()
+    
+    last_char =  branding_text[-1]
+    if last_char not in {"!",".","?"}:
+        branding_text += "..."
+
+    return branding_text
+
+def generate_keyword(user_input: str):
+    
+    openai.api_key = os.environ.get("API_Key")
+ 
+    enriched_prompt = f"Generate related branding keywords for {user_input}:"
+
+    response = openai.Completion.create(model="text-davinci-003", prompt=enriched_prompt, temperature=1.5, max_tokens=32)
+
+    keywords_text: str = response["choices"][0]["text"]
+    
+    keywords_text = keywords_text.strip()
+
+    return keywords_text
 
 if __name__ == "__main__" :
     main()
