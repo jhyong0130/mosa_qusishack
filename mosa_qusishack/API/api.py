b95c2953a88a18c68feccf9e036a7e47e110c295
[junhao yong] <[junhao.yong@aakel.co.jp]>
2023-06-18 12:21:01 +0900

    add feature

--- mosa_qusishack/API/api.py
+++ mosa_qusishack/API/api.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, HTTPException
-from openapi import generate_idea, generate_link
+from openapi import generate_idea, generate_link, generate_jobscope_and_skills
 from fastapi.middleware.cors import CORSMiddleware
 from mangum import Mangum
 
@@ -15,8 +15,8 @@ app.add_middleware(
 )
 
 @app.get("/generate_project_ideas")
-async def get_project_ideas(level_input: str, role_input: str):    
-    ideas_list = generate_idea(level_input, role_input)
+async def get_project_ideas(level_input: str, language_input: str, role_input: str):    
+    ideas_list = generate_idea(level_input, language_input, role_input)
     return {"portfolio_ideas": ideas_list}
 
 
@@ -25,3 +25,7 @@ async def get_link(language_input: str):
     links_list = generate_link(language_input)
     return {"tutorial_links": links_list}
 
+@app.get("/generate_jobscope_and_skills")
+async def get_jobscope_skills(role_input: str):
+    jobscope_and_skills = generate_jobscope_and_skills(role_input)
+    return{"job scope and skills required": jobscope_and_skills}
--- mosa_qusishack/API/openapi.py
+++ mosa_qusishack/API/openapi.py
@@ -6,7 +6,6 @@ import re
 def main():
     
     #get input from user
-    
     parser = argparse.ArgumentParser()
     parser.add_argument("--input", "-i", type=str, required=True)
     args = parser.parse_args()
@@ -23,12 +22,12 @@ def main():
     print(f"Ideas:{ideas}")
     print(f"Useful Links:{tutorial_links}")
 
-def generate_idea(level_input: str, role_input: str) -> str :
+def generate_idea(level_input: str, role_input: str, language_input: str) -> str :
     
     openai.api_key = os.getenv("OPENAI_API_KEY")
     
     #Generate response for ideas
-    prompt = f"Suggest three {level_input} portfolio projects to be a {role_input} engineer(only project name)"
+    prompt = f"Suggest three {level_input} {language_input}portfolio projects to be a {role_input} engineer(only project name)"
     response = openai.Completion.create(model="text-davinci-003", 
                                         prompt=prompt, 
                                         temperature=1,
@@ -46,10 +45,10 @@ def generate_idea(level_input: str, role_input: str) -> str :
 
 def generate_link(language_input :str) -> str :
     
-    openai.api_key = os.getenv("OPENAI_API_KEY")
+    openai.api_key = os.environ.get("OPENAI_API_KEY")
     
     #Generate response
-    enriched_prompt = f"Suggest three best free resource to learn {language_input}(link only)"
+    enriched_prompt = f"Suggest three free online course to learn {language_input}(link only)"
     response = openai.Completion.create(model="text-davinci-003",
                                         prompt=enriched_prompt,
                                         temperature=1,
@@ -67,6 +66,26 @@ def generate_link(language_input :str) -> str :
     tutorial_link_array = [k for k in tutorial_link_array if len(k) > 0]
 
     return tutorial_link_array
+
+def generate_jobscope_and_skills(role_input: str):
+    
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+    
+    prompt = f"Job scope of {role_input} and skillls required to be a{role_input} programmer in 3 sentences"
+    
+    response = openai.Completion.create(model="text-davinci-003",
+                                        prompt=prompt,
+                                        temperature=0,
+                                        max_tokens=256,
+                                        top_p=1,
+                                        best_of=3,
+                                        frequency_penalty=0,
+                                        presence_penalty=0)
+     #Change output to list
+    jobscope_and_skills: str = response["choices"][0]["text"]
+    jobscope_and_skills = jobscope_and_skills.strip()
+    
+    return jobscope_and_skills
 
 if __name__ == "__main__" :
     main()
